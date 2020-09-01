ec2dd329cede778e62205fa2d12ce919399def29
Owner <Owner@DESKTOP-31MLKNN>
2020-09-01 09:02:47 +0800

    penadt

--- public_html/ScriptTest1.js
+++ public_html/ScriptTest1.js
@@ -11,10 +11,15 @@ window.onload = function(){
     document.getElementById("var").innerHTML  = "The answer for x + y = " + z;  
     document.getElementById("name").innerHTML = person.fullName() + "'s ID number is "+person.id;
     document.getElementById("list").innerHTML = text;
-}
+    document.querySelector('h2').innerHTML = localStorage.getItem('count');
+};
 
 function myFunction(){
-    document.getElementById("demo").innerHTML = "Header changed";
+    let heading = document.querySelector('h1');
+    if(heading.innerHTML === 'Fun with JavaScript')
+        heading.innerHTML = "Header changed";
+    else
+        heading.innerHTML = "Fun with JavaScript";
 }
 
 
@@ -30,6 +35,35 @@ function multiplyFunction(para1, para2){
 function displayAnswer(){
     document.getElementById("function-example").innerHTML = "The answer is "+ans;
 }
+
+var count=0;
+
+if(!localStorage.getItem('count')){
+    localStorage.setItem('count',0);
+}
+
+function counter(){
+    let count = localStorage.getItem('count');
+    count++;
+    document.querySelector('h2').innerHTML = count;
+    localStorage.setItem('count', count);
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+   document.getElementById('counter-button').onclick = counter; 
+//   
+//   setInterval(counter,1000);
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    
+   document.querySelectorAll('.color-button').forEach(function(button){
+       button.onclick = function() {
+       document.querySelector('h1').style.color = button.dataset.color;
+        };
+   });
+   
+});
 
 var x,y,z;
 x=5;
